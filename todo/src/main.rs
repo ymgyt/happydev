ccdf325a42e7dcc8648fa687699cbb0dcf711617
ymgyt <yamaguchi7073xtt@gmail.com>
2020-05-22 21:18:54 +0900

    hyperのexampleを試してみる

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,25 +1,121 @@
+use futures_util::TryStreamExt;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
+use url::form_urlencoded;
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn _hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("Hello, World".into()))
+}
+
+async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(Body::from(
+            "Try POSTing data to /echo such as: `curl localhost:3000/echo -XPOST -d 'hello world'",
+        ))),
+
+        (&Method::POST, "/echo") => Ok(Response::new(req.into_body())),
+
+        (&Method::POST, "/echo/uppercase") => {
+            let chunk_stream = req.into_body().map_ok(|chunk| {
+                chunk
+                    .iter()
+                    .map(|byte| byte.to_ascii_uppercase())
+                    .collect::<Vec<u8>>()
+            });
+            Ok(Response::new(Body::wrap_stream(chunk_stream)))
+        }
+
+        (&Method::POST, "/echo/reversed") => {
+            let whole_body = hyper::body::to_bytes(req.into_body()).await?;
+
+            let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
+            Ok(Response::new(Body::from(reversed_body)))
+        }
+
+        _ => {
+            if req.uri().path() == "/form" {
+                form_example(req).await
+            } else {
+                not_found()
+            }
+        },
+    }
+}
+
+fn not_found() -> Result<Response<Body>, hyper::Error> {
+    let mut not_found = Response::default();
+    *not_found.status_mut() = StatusCode::NOT_FOUND;
+    Ok(not_found)
+}
+
+static INDEX: &[u8] = b"<html><body><form action=\"form\" method=\"post\">Name: <input type=\"text\" name=\"name\"><br>Number: <input type=\"text\" name=\"number\"><br><input type=\"submit\"></body></html>";
+static MISSING: &[u8] = b"Missing field";
+static NOTNUMERIC: &[u8] = b"Number field is not numeric";
+
+async fn form_example(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/form") => Ok(Response::new(INDEX.into())),
+        (&Method::POST, "/form") => {
+            let b = hyper::body::to_bytes(req).await?;
+            let params = form_urlencoded::parse(b.as_ref())
+                .into_owned()
+                .collect::<HashMap<String, String>>();
+
+            let name = if let Some(n) = params.get("name") {
+                n
+            } else {
+                return Ok(Response::builder()
+                    .status(StatusCode::UNPROCESSABLE_ENTITY)
+                    .body(MISSING.into())
+                    .unwrap());
+            };
+            let number = if let Some(n) = params.get("number") {
+                if let Ok(v) = n.parse::<f64>() {
+                    v
+                } else {
+                    return Ok(Response::builder()
+                        .status(StatusCode::UNPROCESSABLE_ENTITY)
+                        .body(NOTNUMERIC.into())
+                        .unwrap());
+                }
+            } else {
+                return Ok(Response::builder()
+                    .status(StatusCode::UNPROCESSABLE_ENTITY)
+                    .body(MISSING.into())
+                    .unwrap());
+            };
+
+            let body = format!("Hello {}, your number is {}", name, number);
+            Ok(Response::new(body.into()))
+        },
+        _ => not_found()
+    }
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("Failed to install CTRL+C signal handler");
 }
 
 #[tokio::main]
 async fn main() {
-    let make_svc = make_service_fn(|_conn|  {
-        async {Ok::<_,Infallible>(service_fn(hello_world)) }
-    });
+    pretty_env_logger::init();
 
-    let addr = SocketAddr::from(([127,0,0,1], 3000));
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let server = Server::bind(&addr)
+        .serve(service)
+        .with_graceful_shutdown(shutdown_signal());
 
     println!("Listening on http://{}", addr);
 
     if let Err(e) = server.await {
-        eprintln!("server error: {}",e);
+        eprintln!("server error: {}", e);
     }
 }
