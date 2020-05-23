7190beb80b36cc4e20752a7e5e1ffb9de0d6104d
ymgyt <yamaguchi7073xtt@gmail.com>
2020-05-23 20:10:14 +0900

    cssをmaterial-ui/stylesに移行

--- todo/front/src/App.tsx
+++ todo/front/src/App.tsx
@@ -1,12 +1,18 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Tasks, {TasksProps} from './components/tasks/Tasks';
 import Layout from "./components/layout/Layout";
-import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import {makeStyles} from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  app: {
+    fontFamily: 'Roboto',
+  },
+}));
 
 
 const App = () => {
-
+  const classes = useStyles();
   useEffect(() => {
     console.log("initialize app...");
   })
@@ -23,10 +29,12 @@ const App = () => {
   const [tasks, setTask] = useState(initialTasks);
 
   return (
+    <div className={classes.app}>
       <Layout>
         <CssBaseline/>
         <Tasks {...tasks}/>
       </Layout>
+    </div>
   );
 }
 
